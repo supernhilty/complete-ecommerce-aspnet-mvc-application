577414c5bd244d40ca8ff800d1527eb9e926e904
supernhilty <nhilty302814@gmail.com>
2023-07-07 11:35:52 +0700

    add Data Layer, controller, migration

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Profile Picture URL")]
+        public string? ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; } = null!;
+        [Display(Name = "Biography")]
+        public string? Bio { get; set; }
+
+        public List<Actor_Movie> Actor_Movies { get; set; }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,13 @@
+﻿namespace eTickets.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public string? ProfilePictureURL { get; set; }
+        public string FullName { get; set; } = null!;
+        public string? Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+    }
+}
